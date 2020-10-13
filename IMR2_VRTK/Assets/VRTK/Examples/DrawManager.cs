a1af1464710c2a995ad6316b4e17718b20f2dc5e
Alex Cazan <alexgabriel005@gmail.com>
2020-10-13 19:53:49 +0300

    Lab2IMR

---
+++ IMR2_VRTK/Assets/VRTK/Examples/DrawManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DrawManager : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public GameObject drawingPrefab;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            GameObject drawing = Instantiate(drawingPrefab);
+            drawing.GetComponent<Renderer>().enabled = false;
+            lineRenderer = drawing.GetComponent<LineRenderer>();
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            FreeDraw();
+        }
+    }
+
+    void FreeDraw()
+    {
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+        lineRenderer.positionCount++;
+        lineRenderer.SetPosition(lineRenderer.positionCount-1, drawingPrefab.transform.position);
+    }
+}
