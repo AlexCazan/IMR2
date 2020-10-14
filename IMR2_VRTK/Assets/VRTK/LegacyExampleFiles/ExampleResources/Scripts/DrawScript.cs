35fafcc8a6d5b643298972faa26c579cfeb67c02
Alex Cazan <alexgabriel005@gmail.com>
2020-10-14 21:23:09 +0300

    Lab2Final

---
+++ IMR2_VRTK/Assets/VRTK/LegacyExampleFiles/ExampleResources/Scripts/DrawScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DrawScript : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public GameObject creta;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            GameObject drawing = Instantiate(creta);
+            lineRenderer = drawing.GetComponent<LineRenderer>();
+            drawing.GetComponent<Renderer>().enabled = false;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            Draw();
+        }
+    }
+
+    void Draw()
+    {
+        lineRenderer.startWidth = 0.2f;
+        lineRenderer.endWidth = 0.2f;
+        lineRenderer.positionCount++;
+        lineRenderer.SetPosition(lineRenderer.positionCount-1, creta.transform.position);
+    }
+}
