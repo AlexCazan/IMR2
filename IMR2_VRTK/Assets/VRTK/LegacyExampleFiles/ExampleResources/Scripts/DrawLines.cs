767cdd604855c126f127323a7f1738b2d512096f
Alex Cazan <alexgabriel005@gmail.com>
2020-10-14 22:07:23 +0300

    Rectificare

--- IMR2_VRTK/Assets/VRTK/LegacyExampleFiles/ExampleResources/Scripts/DrawLines.cs
+++ IMR2_VRTK/Assets/VRTK/LegacyExampleFiles/ExampleResources/Scripts/DrawLines.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class DrawScript : MonoBehaviour
+public class DrawLines : MonoBehaviour
 {
     private LineRenderer lineRenderer;
     public GameObject creta;
